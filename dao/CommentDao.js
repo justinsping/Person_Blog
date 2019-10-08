c5fdf209719c3ae518a3c0ad8dd243f5203b71d1
justinsping <justinsp@163.com>
2019-10-08 23:24:27 +0800

    第一次提交

---
+++ dao/CommentDao.js
@@ -0,0 +1,73 @@
+var dbutil = require("./DBUtil");
+
+function insertBlog(title, content, views, tags, ctime, utime, success) {
+    var insertSql = "insert into blog (title, content, views, tags, ctime, utime) values (?, ?, ?, ?, ?, ?)";
+    var params = [title, content, views, tags, ctime, utime];
+    
+    var connection = dbutil.createConnection();
+    connection.connect();
+    connection.query(insertSql, params, function (error, result) {
+        if (error == null) {
+            // console.log(result);
+            success(result);
+        } else {
+            console.log(error)
+        }
+    });
+    connection.end();
+};
+
+function queryBlogByPage(page, pageSize, success) {
+    var querySql = "select * from blog order by id desc limit ?, ?;";
+    var params = [page * pageSize, pageSize];
+
+    var connection = dbutil.createConnection();
+    connection.connect();
+    connection.query(querySql, params, function (error, result) {
+        if (error == null) {
+            success(result);
+        } else {
+            console.log(error)
+        }
+    });
+    connection.end();
+};
+
+function queryBlogById(id, success) {
+    var querySql = "select * from blog where id=?;";
+    var params = [id];
+
+    var connection = dbutil.createConnection();
+    connection.connect();
+    connection.query(querySql, params, function (error, result) {
+        if (error == null) {
+            success(result);
+        } else {
+            console.log(error)
+        }
+    });
+    connection.end();
+};
+
+function queryBlogCount(success) {
+    var querySql = "select count(1) as count from blog;";
+    var params = [];
+
+    var connection = dbutil.createConnection();
+    connection.connect();
+    connection.query(querySql, params, function (error, result) {
+        if (error == null) {
+            success(result);
+        } else {
+            console.log(error)
+        }
+    });
+    connection.end();
+};
+
+module.exports = {
+    insertBlog: insertBlog,
+    queryBlogByPage: queryBlogByPage,
+    queryBlogCount: queryBlogCount,
+    queryBlogById: queryBlogById
+};
